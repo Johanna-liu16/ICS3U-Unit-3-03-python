d1427850b434629b914a2f8ccc2b3b53b13fb509
Johanna Liu <johanna.liu@mths.ca>
2022-10-17 14:39:48 +0000

    init

---
+++ .github/guessing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Johanna Liu
+# Created on: Oct 2022
+# This program adds two numbers
+
+import random
+
+
+def main():
+    # this is a number guessing game
+
+    # input
+    random_number = random.randint(1, 9) # a number between 1 and 9
+    print("Random Number Guessing Game.")
+    user_number = int(input("Enter a number between 0-9: "))
+
+    # process
+    if user_number == random_number:
+        print("YAY! You guessed the correct number!")
+    else:
+        print("Try again.")
+
+    # output
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
